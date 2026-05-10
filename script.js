4c78266144b20a07fc617797807e19cdac22d10d
prettyonabeat <ivnik1488@gmail.com>
2026-05-10 18:26:33 +0300

    Updated puppy website

--- script.js
+++ script.js
@@ -763,7 +763,7 @@ function uniq(arr) {
 function generateTelegramLink(puppy) {
   const message = `Hello! I'm interested in ${puppy.name}, ${puppy.breed}, ${puppy.age}, price ${puppy.price}. Please contact me.`;
   const encodedMessage = encodeURIComponent(message);
-  return `https://t.me/Pomeranianpuppies_mini_mishki?text=${encodedMessage}`;
+  return `https://t.me/pomeranian_mini_mishki?text=${encodedMessage}`;
 }
 
 function setupFinder() {
